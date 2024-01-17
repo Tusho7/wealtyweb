826b7c03f121e67d9bbe9aab069b400af20939e5
mariamdatuka <mariam.datukishvili.7@gmail.com>
2024-01-17 03:38:07 -0800

    feat:add slider

---
+++ components/HomeFour/components/RunningSlider.js
@@ -0,0 +1,34 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/autoplay";
+import SwiperCore from "swiper";
+import { Autoplay } from "swiper";
+SwiperCore.use([Autoplay]);
+
+const RunningSlider = () => {
+  return (
+    <>
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={3}
+        loop={true}
+        autoplay={{
+          delay: 1000,
+          pauseOnMouseEnter: true,
+        }}
+        speed={2000}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
+    </>
+  );
+};
+
+export default RunningSlider;
